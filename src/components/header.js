40e55c8ccb50a67c15e10a31956783ea78d79d39
Asmit <iasmiti19@gmail.com>
2023-08-19 15:37:27 +0530

    Updates

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import "../sheets/header.css";
 export const Header = () => {
     return (
         <div className="header">
-            <h1>Knote</h1>
+            <h1>Knote - <span className="app-name">Task Manager</span></h1>
         </div>
     );
 };
